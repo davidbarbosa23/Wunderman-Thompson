76327b6ded0f54722a4faa30c9ff66b674069d54
David Barbosa <davidbarbosa.dev@gmail.com>
2020-10-18 10:36:53 -0500

    HomePage Content

--- client/src/app/components/homepage/homepage.component.ts
+++ client/src/app/components/homepage/homepage.component.ts
@@ -1,16 +1,38 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ItemsService } from '../../services/items.service';
+import { Item } from '../../models/Item';
+
+import { Globals } from '../../shared/globals';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent implements OnInit {
   @HostBinding('class') classes = 'row';
 
-  constructor() { }
+  items: Item[] = [];
+
+  constructor(
+    private itemsService: ItemsService,
+    public globals: Globals,
+    public router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.getItems();
   }
 
+  getItems() {
+    this.itemsService.getItems().subscribe(
+      (result: []) => {
+        this.items = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
