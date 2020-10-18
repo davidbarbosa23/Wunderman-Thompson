7af603e449dd9895190d7d9559c5444b3cc89789
David Barbosa <davidbarbosa.dev@gmail.com>
2020-10-18 12:02:45 -0500

    NG API_URI

--- client/src/app/shared/globals.ts
+++ client/src/app/shared/globals.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-  API_URI: string = 'https://wunderman-thompson.test/api';
+  API_URI: string = 'http://127.0.0.1:8000/api';
   isSignedIn: boolean = false;
 }
