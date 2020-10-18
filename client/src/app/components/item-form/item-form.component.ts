2a3a11ed48b2a911ff1a572d17f642c31e4e31a3
David Barbosa <davidbarbosa.dev@gmail.com>
2020-10-18 02:20:53 -0500

    Item Form | Create function

--- client/src/app/components/item-form/item-form.component.ts
+++ client/src/app/components/item-form/item-form.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemsService } from './../../services/items.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-item-form',
   templateUrl: './item-form.component.html',
-  styleUrls: ['./item-form.component.scss']
+  styleUrls: ['./item-form.component.scss'],
 })
 export class ItemFormComponent implements OnInit {
+  itemForm: FormGroup;
+  errors = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public fb: FormBuilder, public itemsService: ItemsService) {
+    this.itemForm = this.fb.group({
+      title: [''],
+      photo: [''],
+      description: [''],
+    });
   }
 
+  ngOnInit() {}
+
+  onSubmit() {
+    this.itemsService.createItem(this.itemForm.value).subscribe(
+      (result) => {
+        console.log(result);
+      },
+      (error) => {
+        this.errors = error.error;
+      },
+      () => {
+        // this.itemForm.reset();
+      }
+    );
+  }
 }
